7266435d4056351c709414aa6f25f7fb04462360
Alex Chavez <alexchavez@yahoo.com>
2024-10-02 20:40:05 -0700

    Reapply "dilemma and charybdis remove redundant mouse layer"
    This reverts commit cd5c7b435505aaa8850302bc9b4f6db83f0c533f.

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/act/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/act/keymap.c
@@ -14,7 +14,6 @@ enum dilemma_keymap_layers {
     LYR_SYM,
     LYR_NUF,
     LYR_FUN,
-    LYR_MSE,
 };
 
 //Combo to switch to switch BASE (Canary), ALT (Qwerty), TAP Game (Qwerty)
@@ -33,7 +32,6 @@ combo_t key_combos[] = {
 #define ENT_SYM LT(LYR_SYM, KC_ENT)
 #define ENT_NUF LT(LYR_NUF, KC_ENT)
 #define BSP_FUN LT(LYR_FUN, KC_BSPC)
-#define _L_PTR(KC) LT(LYR_MSE, KC)
 
 // Define clipboard behavior. Current = Windows //
 #define U_RDO C(KC_Y)
@@ -122,12 +120,6 @@ static uint16_t auto_pointer_layer_timer = 0;
     KC_F10,   KC_F1,   KC_F2,   KC_F3,  KC_PAUS, ______________HOME_ROW_GACS_R______________, \
                       KC_ESC,  KC_APP,   KC_TAB,  XXXXXXX, _______, XXXXXXX
 
-#define LAYOUT_LYR_MSE                                                                        \
-   S_D_RMOD, S_D_MOD,DPI_RMOD, DPI_MOD, XXXXXXX, QK_BOOT,  EE_CLR,  QK_RBT, XXXXXXX, XXXXXXX, \
-    KC_WBAK, KC_BTN2, DRGSCRL, KC_BTN1, KC_WFWD, KC_WBAK, KC_BTN1, DRGSCRL, KC_BTN2, KC_WFWD, \
-    ______________HOME_ROW_GACS_L______________, ______________HOME_ROW_GACS_R______________, \
-                      XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN3, KC_BTN1, XXXXXXX
-
 #define _HOME_ROW_MOD_GACS(                                            \
     L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
     L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
@@ -142,25 +134,11 @@ static uint16_t auto_pointer_layer_timer = 0;
       __VA_ARGS__
 #define HOME_ROW_MOD_GACS(...) _HOME_ROW_MOD_GACS(__VA_ARGS__)
 
-#define _POINTER_MOD(                                                  \
-    L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
-    L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
-    L20, L21, L22, L23, L24, R25, R26, R27, R28, R29,                  \
-    ...)                                                               \
-             L00,         L01,         L02,         L03,         L04,  \
-             R05,         R06,         R07,         R08,         R09,  \
-      _L_PTR(L10),        L11,         L12,         L13,         L14,  \
-             R15,         R16,         R17,         R18,  _L_PTR(R19), \
-             L20,         L21,         L22,         L23,         L24,  \
-             R25,         R26,         R27,         R28,         R29,  \
-      __VA_ARGS__
-#define POINTER_MOD(...) _POINTER_MOD(__VA_ARGS__)
-
 #define LAYOUT_wrapper(...) LAYOUT_split_3x5_3(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LYR_BSE] = LAYOUT_wrapper(POINTER_MOD(HOME_ROW_MOD_GACS(LAYOUT_LYR_BSE))),
-  [LYR_ALT] = LAYOUT_wrapper(POINTER_MOD(HOME_ROW_MOD_GACS(LAYOUT_LYR_ALT))),
+  [LYR_BSE] = LAYOUT_wrapper(HOME_ROW_MOD_GACS(LAYOUT_LYR_BSE)),
+  [LYR_ALT] = LAYOUT_wrapper(HOME_ROW_MOD_GACS(LAYOUT_LYR_ALT)),
   [LYR_TAP] = LAYOUT_wrapper(LAYOUT_LYR_TAP),
   [LYR_UTL] = LAYOUT_wrapper(LAYOUT_LYR_UTL),
   [LYR_NAV] = LAYOUT_wrapper(LAYOUT_LYR_NAV),
@@ -168,7 +146,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LYR_NUF] = LAYOUT_wrapper(LAYOUT_LYR_NUF),
   [LYR_SYM] = LAYOUT_wrapper(LAYOUT_LYR_SYM),
   [LYR_FUN] = LAYOUT_wrapper(LAYOUT_LYR_FUN),
-  [LYR_MSE] = LAYOUT_wrapper(LAYOUT_LYR_MSE),
 };
 // clang-format on
 
@@ -177,7 +154,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     if (abs(mouse_report.x) > DILEMMA_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > DILEMMA_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
         if (auto_pointer_layer_timer == 0) {
-            layer_on(LYR_MSE);
+            layer_on(LYR_NVE);
 #        ifdef RGB_MATRIX_ENABLE
             rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
             rgb_matrix_sethsv_noeeprom(HSV_GREEN);
@@ -191,7 +168,7 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 void matrix_scan_user(void) {
     if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= DILEMMA_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
         auto_pointer_layer_timer = 0;
-        layer_off(LYR_MSE);
+        layer_off(LYR_NVE);
 #        ifdef RGB_MATRIX_ENABLE
         rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
 #        endif // RGB_MATRIX_ENABLE
